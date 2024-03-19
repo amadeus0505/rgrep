7459cc5e12fa4b8bbacde4413e48a2dd1146686a
amadeus0505 <42494998+amadeus0505@users.noreply.github.com>
2024-03-19 11:49:43 +0100

    added config struct

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,39 @@
-use std::io::{self, BufRead, Error, IsTerminal, Read, ErrorKind};
-use std::env;
-use colors::{cprint, Color};
-use colored::*;
+use std::io::{self, Error, IsTerminal, Read};
+use std::{env, process};
 mod colors;
 
-fn main() -> io::Result<()> {
-    
+struct Config {
+    search_string: String,
+    from_file: bool,
+}
+
+fn main(){
+
+    // get command line arguments
     let args = env::args().collect::<Vec<String>>();
+    // 
     let search_term = match args.get(1) {
         Some(arg) => arg,
         None => {
             eprintln!("Error: Missing search term. Please provide a search term as the first argument.");
-            return Err(Error::new(ErrorKind::InvalidInput, "Missing search term"));
+            process::exit(1);
         }
     };
-    match rgrep(search_term) {
-        Ok(_) => Ok(()),
-        Err(e) => Err(e),
+    let config = Config {
+        search_string: search_term.to_string(),
+        from_file: false,
+    };
+    if let Err(e) = rgrep(config){
+        eprintln!("Error: {}", e);
     }
+    
 }
 
-fn rgrep(search_string: &str) -> Result<(), Error> {
-
+fn rgrep(config: Config) -> Result<(), Error> {
     let data = get_piped_data()?;
     
     // find all indices of 
-    let indices: Vec<_> = data.match_indices(&search_string).collect();
+    let indices: Vec<_> = data.match_indices(&config.search_string).collect();
 
     let mut prev = 0;
     for (index, string) in indices{
@@ -47,10 +55,9 @@ fn get_piped_data() -> Result<String, Error> {
     let mut handle = stdin.lock();
 
     if stdin.is_terminal() {
-        eprint!("No piped data found");
         return Err(Error::new(io::ErrorKind::InvalidInput, "No piped data found"));
     } else {
         let _ = handle.read_to_string(&mut buffer);
         return Ok(buffer);
-    } 
+    }
 }
